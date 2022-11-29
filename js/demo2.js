ad275ee99e86734414e88ac6eb941ba0b774d946
Pajeee <marundan.f.d123@gmail.com>
2022-11-30 04:11:59 +0800

    commit 6

---
+++ js/demo2.js
@@ -0,0 +1,24 @@
+//setup
+const data = {
+    labels: ['Female', 'Male', ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [142, 136],
+        backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+        ],
+    }]
+};
+
+//config
+const config = {
+    type: 'pie',
+    data: data,
+};
+
+//render init
+const pie = new Chart(
+    document.getElementById('pie'),
+    config
+);
--- js/download.js
+++ js/download.js
@@ -1,17 +1,18 @@
 Chart.defaults.font.size = 25;
 //setup
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sad', 'Sun'],
         datasets: [{
             label: 'Number of Downloads',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3, 14],
             backgroundColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
+                'rgba(51, 202, 27, 1)'
             ],
         }]
 };
